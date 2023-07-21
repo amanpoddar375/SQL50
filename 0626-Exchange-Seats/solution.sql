904d53330b52f8d5d986c7c376fbb3962041e2ca
amanpoddar375 <amanpoddar5566@gmail.com>
2023-07-21 23:27:00 +0545

    added sql solution

---
+++ 0626-Exchange-Seats/solution.sql
@@ -0,0 +1,8 @@
+SELECT
+    CASE 
+        WHEN id%2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id 
+        WHEN id%2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,student
+FROM Seat
+ORDER BY id;
